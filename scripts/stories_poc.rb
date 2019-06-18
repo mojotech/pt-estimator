9c5155c043ca2056bb33ec2fea82541959add9b4
Mariana Pachon <mariana@mojotech.com>
2019-06-18 16:43:15 -0400

    feat: set valid estimate for stories

--- scripts/stories_poc.rb
+++ scripts/stories_poc.rb
@@ -13,6 +13,30 @@ def get_story(project_id, story_id = nil)
   create_http_req(uri, request)
 end
 
+def put_story_estimate(project_id, story_id, estimate)
+  allowed, api_token = allow_estimate?(project_id, estimate)
+
+  return "Estimate of #{estimate} not allowed in point scale" unless allowed
+
+  uri = URI.parse('https://www.pivotaltracker.com/services/v5/projects/'\
+    "#{project_id}/stories/#{story_id}")
+  request = Net::HTTP::Put.new(uri)
+  request.content_type = 'application/json'
+  request.body = JSON.dump('estimate' => estimate)
+  request['X-Trackertoken'] = api_token
+  create_http_req(uri, request)
+end
+
+def allow_estimate?(project_id, estimate)
+  uri = URI.parse('https://www.pivotaltracker.com/services/v5/projects/' \
+     "#{project_id}")
+  request = Net::HTTP::Get.new(uri)
+  api_token = auth_user['api_token']
+  request['X-Trackertoken'] = api_token
+  scale = create_http_req(uri, request)['point_scale'].split(',').map(&:to_i)
+  [scale.include?(estimate), api_token]
+end
+
 def create_http_req(uri, request)
   req_options = { use_ssl: uri.scheme == 'https' }
   response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
@@ -45,3 +69,5 @@ project_id = '2354450'
 poc_story_id = '166701587'
 puts get_story(project_id) # Get all stories
 puts get_story(project_id, poc_story_id) # Get one story only
+puts put_story_estimate(project_id, poc_story_id, 2) # Estimate of 2 on a story
+puts put_story_estimate(project_id, poc_story_id, 4) # Not allowed in fib scale
