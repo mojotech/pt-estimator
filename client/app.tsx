85049176c3fc40b27b7455a1cf15843a3068f44a
Craig Barber <craigb@mojotech.com>
2019-06-27 08:52:51 -0400

    chore: connect client and server in development
    The goal here is a single command to start up parcel proxying to rails.
    package.json happenned to be in the root so I went with concurrently
    over foreman but have no real preference if we want to move things
    around.

--- client/app.tsx
+++ client/app.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useQuery } from 'urql';
 
 interface Props {
   message: string;
 }
 
-class App extends React.Component<Props> {
-  render() {
-    return <p>{this.props.message}</p>;
-  }
-}
+const App: React.SFC<Props> = ({ message }) => {
+  const [result] = useQuery({
+    query: `{ testField }`,
+  });
+  const { data } = result;
+
+  return <div>
+    <p>{message}</p>
+    <p>and...</p>
+    {data && <p>{data.testField}</p>}
+  </div>;
+};
 
 export default App;
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-"$(yarn bin)/tsc" --noEmit --watch &
-"$(yarn bin)/parcel" -p 3000 ./client/index.html
