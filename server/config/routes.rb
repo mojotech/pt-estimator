ebaee495871a4a0b45d995a61d7a75a1a220a5ae
Mariana Pachon <mariana@mojotech.com>
2019-07-01 10:16:16 -0400

    chore: add graphiql interface

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    puts "DEVELOPING"
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  end
   post "/graphql", to: "graphql#execute"
 end
