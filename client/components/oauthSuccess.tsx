c04eec6a2919f326fb62bc0e5e4c65b6f53de429
Mariana Pachon <mariana@mojotech.com>
2019-07-09 14:46:13 -0400

    feat: add gql query for user on auth success

--- client/components/oauthSuccess.tsx
+++ client/components/oauthSuccess.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useStore } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { useQuery } from 'urql';
+import { omit } from 'ramda';
 
-import { State } from '../redux/reducers';
-import { User, Login } from '../redux/types';
+import { setToken } from '../redux/actions/user';
 import TokenPrompt from '~components/tokenPrompt';
+import Homepage from '~components/Homepage';
 
-interface Props {
-  login: Login;
-  user: User;
+const fetchUser = `
+query FetchUser($data: UserInput!){
+  fetchUser(data: $data){
+    email
+    firstName
+    id
+    imgUrl
+    lastName
+    name
+    apiToken
+  }
+}
+`;
+
+function OAuthSuccess() {
+  const store = useStore();
+  const state = store.getState();
+  const dispatch = useDispatch();
+
+  const [res] = useQuery({
+    query: fetchUser,
+    variables: { data: omit(['id', 'apiToken'], state.userReducer) },
+  });
+
+  if (res.fetching) {
+    return <>Loading...</>;
+  } else if (res.error) {
+    return <>Error</>;
+  } else if (res.data.fetchUser.apiToken) {
+    // dispatch(setToken(res.data.fetchUser.apiToken));
+  }
+
+  return state.loginReducer.loggedIn ? (
+    res.data.fetchUser.apiToken ? (
+      <Homepage />
+    ) : (
+      <TokenPrompt />
+    )
+  ) : (
+    <Redirect to="/failure" />
+  );
 }
 
-function OAuthSuccess(props: Props) {
-  return props.login.loggedIn ? <TokenPrompt /> : <Redirect to="/failure" />;
-}
-
-const mapStateToProps = function(state: State) {
-  return {
-    login: state.loginReducer,
-  };
-};
-
-export default connect(mapStateToProps)(OAuthSuccess);
+export default OAuthSuccess;
