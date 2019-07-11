66a61590ff386c4bf0a8ca80df3491532960723a
Mariana Pachon <mariana@mojotech.com>
2019-07-11 16:00:28 -0400

    feat: validate token input in form

--- client/components/oauth/tokenPrompt.tsx
+++ client/components/oauth/tokenPrompt.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { useStore } from 'react-redux';
+import { useQuery } from 'urql';
 
-const TokenPrompt = () => (
-  <>
-    API Token: <input />
-  </>
-);
+import Homepage from '~components/Homepage';
+
+const validateToken = `query validateToken($email: String!, $token: String!){
+  validity(email: $email, token: $token)
+}`;
+
+const TokenPrompt = () => {
+  const state = useStore().getState();
+
+  const [token, setToken] = useState();
+  const tokenRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit = () => {
+    event.preventDefault();
+    setToken(tokenRef.current.value);
+  };
+
+  const [res] = useQuery({
+    query: validateToken,
+    variables: {
+      email: state.email,
+      token: token,
+    },
+  });
+
+  if (res.data && res.data.validity) {
+    return <Homepage />;
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input required ref={tokenRef} />
+      <input type="submit" />
+    </form>
+  );
+};
 
 export default TokenPrompt;
