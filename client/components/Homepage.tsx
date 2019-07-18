abfe0b46c8e7acd49ec332b1677a5760b0577485
Mariana Pachon <mariana@mojotech.com>
2019-07-18 12:07:43 -0400

    feat: add comment from frontend

--- client/components/Homepage.tsx
+++ client/components/Homepage.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 import Projects from '~components/projects/projects';
 
-const Homepage = () => (
-  <>
-    <>Token successful, now in Homepage</>
-    <Projects />
-  </>
-);
+const Homepage = () => <Projects />;
 
 export default Homepage;
