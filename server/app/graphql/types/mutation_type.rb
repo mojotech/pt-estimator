b1db332f0fa3ebca384fbfa0e7ea7088c82faae9
Mariana Pachon <mariana@mojotech.com>
2019-07-18 11:01:49 -0400

    feat: add comment to story through gql query

--- server/app/graphql/types/mutation_type.rb
+++ server/app/graphql/types/mutation_type.rb
@@ -1,10 +1,5 @@
 module Types
-  class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+  class MutationType < BaseObject
+    field :create_comment, mutation: Mutations::CreateStoryComment
   end
 end
