afa626ce2386db9accf7565455ee793169343696
Mariana Pachon <mariana@mojotech.com>
2019-07-22 10:10:44 -0400

    feat: store estimate of a story for a user

--- server/app/models/story.rb
+++ server/app/models/story.rb
@@ -2,6 +2,7 @@
 
 class Story < ApplicationRecord
   belongs_to :project
+  has_many :estimates, dependent: :destroy
 
   def self.filter_stories_to_review(pt_project:, db_project:, filter:)
     pt_project.stories(filter: filter).map do |s|
@@ -26,7 +27,35 @@ class Story < ApplicationRecord
     end .compact          
 
     st = pt_story.to_h.slice(:id, :name, :description, :story_type, :estimate)
-    st.merge!(tasks: tasks, comments: comms, labels: labels)
+    st.merge!(tasks: tasks, comments: comms, labels: labels, user_estimates: self.estimates)
+  end
+
+  def self.save_estimate(story_id:, point_value:, user_email:)
+    user = User.find_by(email: user_email)
+    return { message: 'User not found' } if user.nil?
+
+    story = Story.find_by(id: story_id)
+    return { message: 'Story not found' } if story.nil?
+
+    if valid_point_value?(point_value: point_value, story: story, user: user)
+      est = user.estimates.find_by(story_id: story_id, user_id: user.id)
+      if est.nil?
+      est = user.estimates.create!(story_id: story_id, user_id: user.id, point_value: point_value)
+      else
+        est.update(point_value: point_value)
+      end
+      
+      est
+    else 
+      { message: 'Invalid point value for this project\'s schema' }
+    end
+  end
+
+  def self.valid_point_value?(point_value:, story:, user:)
+    project_id = story[:project_id]
+    token = user[:api_token]
+    Project.get_valid_estimates(token: token, project_id: project_id)
+           .include?(point_value)
   end
 
   def self.sort_comments(pt_coms:)
@@ -42,5 +71,10 @@ class Story < ApplicationRecord
     client = TrackerApi::Client.new(token: token)
     build_comment(com: client.story(story_id).create_comment(text: text))
     client.story(story_id)
+  end
+  
+  def self.get_story_estimates(story_id:)
+    story = Story.find_by(id: story_id)
+    story&.estimates
   end
 end
