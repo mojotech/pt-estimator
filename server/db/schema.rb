5071c6aff67591a737af4504f4198a5a0e65763c
Mariana Pachon <mariana@mojotech.com>
2019-07-22 10:10:10 -0400

    chore: create estimate model

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 2019_07_31_152243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "estimates", force: :cascade do |t|
+    t.integer "point_value", null: false
+    t.bigint "story_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["story_id"], name: "index_estimates_on_story_id"
+    t.index ["user_id"], name: "index_estimates_on_user_id"
+  end
+
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
