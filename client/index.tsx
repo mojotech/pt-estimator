21b3b7d7efb8c3f4c575d48ed8581b00f0149fa5
Mariana Pachon <mariana@mojotech.com>
2019-07-26 12:20:21 -0400

    feat: store jwt in cookie and validate

--- client/index.tsx
+++ client/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider as UrqlProvider, createClient } from 'urql';
 import { Provider as ReduxProvider } from 'react-redux';
+import { CookiesProvider } from 'react-cookie';
 
 const client = createClient({
   url: '/graphql',
+  fetchOptions: { credentials: 'include' },
 });
 
 import App from '~/app';
@@ -12,11 +14,13 @@ import store from './redux/reducers';
 
 const render = () => {
   ReactDOM.render(
-    <ReduxProvider store={store}>
-      <UrqlProvider value={client}>
-        <App />
-      </UrqlProvider>
-    </ReduxProvider>,
+    <CookiesProvider>
+      <ReduxProvider store={store}>
+        <UrqlProvider value={client}>
+          <App />
+        </UrqlProvider>
+      </ReduxProvider>
+    </CookiesProvider>,
     document.getElementById('root')
   );
 };
