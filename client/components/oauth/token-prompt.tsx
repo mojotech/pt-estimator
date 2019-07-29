a6a63d0d5ad3b625e695af8784f0f278d5536c74
Simon Barber <simon@mojotech.com>
2019-07-29 13:51:17 -0400

    refactor: move input field to tokenPrompt

--- client/components/oauth/token-prompt.tsx
+++ client/components/oauth/token-prompt.tsx
@@ -1,18 +1,47 @@
 import React, { useRef, useState } from 'react';
 import { useStore } from 'react-redux';
+import styled from 'styled-components';
 import { useQuery } from 'urql';
 
 import Homepage from '~components/Homepage';
 
+import { colors, fonts, fontSizes } from '~lib/theme';
+
 const validateToken = `query validateToken($token: String!){
   isApiTokenValid(token: $token)
 }`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 40px;
+`;
+
+const Input = styled.input`
+  type: text;
+  width: 391px;
+  height: 60px;
+  font-family: ${fonts.neueHass};
+  opacity: 0.5;
+  font-size: ${fontSizes.medium};
+  line-height: 1.31;
+  color: ${colors.black};
+`;
+
+const AcceptBox = styled.input`
+  width: 60px;
+  height: 60px;
+  border: solid 1px #d3d2d2;
+  background-color: #efefef;
+`;
 
 const TokenPrompt = () => {
   const state = useStore().getState();
 
   const [token, setToken] = useState();
   const tokenRef = useRef<HTMLInputElement>(null);
+
+  const placeholder = 'API token';
 
   const onSubmit = event => {
     event.preventDefault();
@@ -31,10 +60,12 @@ const TokenPrompt = () => {
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <input required ref={tokenRef} />
-      <input type="submit" />
-    </form>
+    <Wrapper>
+      <form onSubmit={onSubmit}>
+        <Input placeholder={placeholder} required ref={tokenRef} />
+        <AcceptBox type="submit" />
+      </form>
+    </Wrapper>
   );
 };
 
