45fa9f841ced664573c42d6cee26845ff1b37912
Mariana Pachon <mariana@mojotech.com>
2019-07-31 19:18:36 -0400

    chore: set up redux state for story

--- client/redux/reducers/index.ts
+++ client/redux/reducers/index.ts
@@ -2,11 +2,13 @@ import { combineReducers, createStore } from 'redux';
 import { user } from './user';
 import { project } from './project';
 import { toggleStory } from './toggle-story-list';
+import { story } from './story';
 
 const reducers = combineReducers({
   user,
   project,
   toggleStory,
+  story,
 });
 
 const store = createStore(reducers);
---
+++ client/redux/reducers/story.ts
@@ -0,0 +1,34 @@
+import { Story } from '~components/projects/types';
+
+interface StoryAction {
+  type: 'SET_STORY';
+  story: Story;
+  storyPosition: number;
+}
+
+const initialState = {
+  story: {
+    comments: null,
+    description: null,
+    estimate: null,
+    id: null,
+    name: null,
+    storyType: null,
+    tasks: null,
+    labels: null,
+  },
+  storyPosition: null,
+};
+
+export const story = (state = initialState, action: StoryAction) => {
+  switch (action.type) {
+    case 'SET_STORY':
+      return {
+        ...state,
+        story: { ...action.story },
+        storyPosition: action.storyPosition,
+      };
+    default:
+      return state;
+  }
+};
