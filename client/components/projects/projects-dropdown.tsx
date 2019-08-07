2fa8d1f20366b624c59c8cf028c7beca6c4fd85e
Simon Barber <simon@mojotech.com>
2019-08-07 16:11:34 -0400

    feat: style project dropdown

--- client/components/projects/projects-dropdown.tsx
+++ client/components/projects/projects-dropdown.tsx
@@ -3,26 +3,44 @@ import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 
 import * as Types from '~components/projects/types';
-import { colors, fontSizes } from '~lib/theme';
+import { colors, fonts, fontSizes } from '~lib/theme';
 import { setProject } from '~redux/actions/project';
 import { setStory } from '~redux/actions/story';
 
-const ProjectSelect = styled.select`
-  > img {
-    padding: 10px 7px;
+const Rectangle = styled.button`
+  width: 320px;
+  height: 57px;
+  outline: none;
+  background-color: ${colors.charcoal};
+  :hover {
+    background-color: ${colors.warmGrey};
   }
+`;
 
-  display: flex;
+const RectangleText = styled.div`
+  font-family: ${fonts.neueHass};
+  font-size: ${fontSizes.small};
+  line-height: 1.29;
   color: ${colors.white};
-  font-size: ${fontSizes.medium};
-  font-family: GT America;
-  align-items: center;
-  justify-content: center;
-  background: ${colors.charcoal};
-  border: none;
-  appearance: none;
-  padding: 10px 7px;
-  position: relative;
+  text-align: left;
+  margin-left: 24px;
+`;
+
+const Column = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  z-index: 15;
+`;
+
+const Divider = styled.div`
+  position: absolute;
+  left: 24px
+  margin-top: 18px;
+  width: 296px;
+  height: 1px;
+  opacity: 0.2;
+  background-color: ${colors.white};
 `;
 
 interface Props {
@@ -32,9 +50,9 @@ interface Props {
 const ProjectsDropdown = ({ projects }: Props) => {
   const dispatch = useDispatch();
 
-  const onChange = event => {
+  const onClick = event => {
     event.preventDefault();
-    const newProject = projects.find(pr => pr.id === event.target.value);
+    const newProject = projects.find(pr => pr.id === event.currentTarget.id);
     dispatch(setProject(newProject));
     dispatch(setStory(newProject.stories[0], 1));
   };
@@ -45,15 +63,17 @@ const ProjectsDropdown = ({ projects }: Props) => {
   }, []);
 
   return (
-    <ProjectSelect onChange={onChange}>
-      {projects.map(project => {
+    <Column>
+    {projects.map((project, index) => {
+      const projectId = project.id.toString(10);
         return (
-          <option key={project.id} value={project.id}>
-            {project.name}
-          </option>
+          <Rectangle key={project.id} id={projectId} onClick={onClick}>
+            <RectangleText> {project.name} </RectangleText>
+            {index !== projects.length - 1 && <Divider />}
+          </Rectangle>
         );
       })}
-    </ProjectSelect>
+    </Column>
   );
 };
 
